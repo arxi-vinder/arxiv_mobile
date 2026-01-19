1676b1966052ab3c9324d136c677efac7d9bc608
KrishnanditoLksn <krislaksana123@gmail.com>
2026-01-19 23:53:58 +0700

    feat:mhome screen init

---
+++ lib/ui/pages/home/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
