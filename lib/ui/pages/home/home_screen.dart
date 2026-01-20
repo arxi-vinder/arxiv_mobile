790fe3b0d3f01f81e5e40e40454384267018c7f5
KrishnanditoLksn <krislaksana123@gmail.com>
2026-01-20 14:16:12 +0700

    style::add home container with decoration

--- lib/ui/pages/home/home_screen.dart
+++ lib/ui/pages/home/home_screen.dart
@@ -12,6 +12,46 @@ class HomeScreen extends StatefulWidget {
 class HomeState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: screenWidth,
+            height: 298,
+            decoration: ShapeDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(0.50, 0.18),
+                end: Alignment(0.50, 1.00),
+                colors: [const Color(0xFF00FFCC), const Color(0xFF00FFCC)],
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(32),
+                  bottomRight: Radius.circular(32),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              SizedBox(height: 100),
+              Container(
+                margin: EdgeInsets.only(
+                  left: 21
+                ),
+                child: Row(
+                  children: [
+                    Text("a"), 
+                    Text("b")
+                  ]
+                )
+            ),
+              Text("abb"),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
