70ddc2c083d352620aa7ec8ef7c1fc78dcbf88cd
KrishnanditoLksn <krislaksana123@gmail.com>
2026-02-09 23:13:57 +0700

    style:add border for custom list

--- lib/ui/utils/custom_list_tile.dart
+++ lib/ui/utils/custom_list_tile.dart
@@ -15,18 +15,25 @@ class CustomListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.blueGrey, width: 2.0),
+        borderRadius: BorderRadius.circular(12.0),
+      ),
       elevation: 0,
       color: Colors.transparent,
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        padding: const EdgeInsets.symmetric(vertical: 11, horizontal: 12),
         child: Column(
           children: [
             Text(
               subTitle,
               style: const TextStyle(
-                fontSize: 16,
+                fontSize: 12,
+                fontFamily: 'Poppins',
                 fontWeight: FontWeight.w500,
                 color: Colors.black,
+                height: 1.33,
+                letterSpacing: 0.50,
               ),
             ),
 
@@ -35,9 +42,10 @@ class CustomListTile extends StatelessWidget {
             Text(
               title,
               style: const TextStyle(
-                fontSize: 24,
+                fontSize: 16,
                 fontWeight: FontWeight.bold,
                 color: Colors.black,
+                height: 1.50,
                 letterSpacing: -0.5,
               ),
             ),
@@ -45,9 +53,10 @@ class CustomListTile extends StatelessWidget {
             Text(
               description,
               style: const TextStyle(
-                fontSize: 16,
+                fontSize: 14,
                 color: Colors.black87,
-                height: 1.4,
+                height: 1.43,
+                letterSpacing: 0.25,
               ),
               maxLines: 4,
               overflow: TextOverflow.ellipsis,
