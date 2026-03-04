beaa5469af3e58c62c8ca0f2c5dbfb9e96de2903
KrishnanditoLksn <krislaksana123@gmail.com>
2026-03-04 08:55:20 +0700

    feat:auth custom component

---
+++ lib/ui/utils/custom_textfield.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class AuthTextField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController? controller;
+  final bool isPassword;
+  final TextInputType keyboardType;
+
+  const AuthTextField({
+    super.key,
+    required this.hintText,
+    this.controller,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: isPassword,
+      keyboardType: keyboardType,
+      style: const TextStyle(fontSize: 16, color: Colors.black),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(color: Colors.black54, fontSize: 16),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 18,
+        ),
+
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(color: Color(0xFF333333), width: 1),
+        ),
+
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(color: Color(0xFF333333), width: 1.5),
+        ),
+
+        filled: true,
+        fillColor: Colors.white,
+      ),
+    );
+  }
+}
