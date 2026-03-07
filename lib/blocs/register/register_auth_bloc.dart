ffd43d32c245f95dd0c3de5558a1fbac8e7b3b3a
KrishnanditoLksn <krislaksana123@gmail.com>
2026-03-07 23:30:43 +0700

    feat:Add bloc for register

---
+++ lib/blocs/register/register_auth_bloc.dart
@@ -0,0 +1,47 @@
+import 'package:arxivinder/blocs/register/register_auth_event.dart';
+import 'package:arxivinder/blocs/register/state_register_auth.dart';
+import 'package:arxivinder/data/services/auth_api.dart';
+import 'package:arxivinder/data/services/secure_storage_service.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RegisterAuthBloc extends Bloc<RegisterAuthEvent, StateRegisterAuth> {
+  final AuthApi authApi;
+
+  RegisterAuthBloc({required this.authApi}) : super(RegisterInitial()) {
+    on<RegisterSubmitted>(_onRegisterSubmitted);
+    // on<RegisterReset>(_onRegisterReset);
+  }
+
+  Future<void> _onRegisterSubmitted(
+    RegisterSubmitted event,
+    Emitter<StateRegisterAuth> emit,
+  ) async {
+    emit(RegisterLoading());
+
+    try {
+      final response = await authApi.register(event.authRequest);
+      debugPrint('Register response: $response');
+
+      if (response.status == 'success' || response.status == '200') {
+        await SecureStorageService.saveUserData(
+          response.accessToken,
+          response.username,
+          response.id.toString(),
+        );
+        emit(RegisterSuccess(response.message));
+      } else {
+        emit(RegisterFailure(response.message));
+      }
+    } catch (e) {
+      emit(RegisterFailure(e.toString()));
+    }
+  }
+
+  // Future<void> _onRegisterReset(
+  //   RegisterReset event,
+  //   Emitter<StateRegisterAuth> emit,
+  // ) async {
+  //   emit(RegisterInitial());
+  // }
+}
