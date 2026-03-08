45defe76a16b2adbc9546e64572001c3713a99c4
KrishnanditoLksn <krislaksana123@gmail.com>
2026-03-08 14:11:17 +0700

    feat:redirect to login page after success registered

--- lib/ui/pages/auth/register/register_page_screen.dart
+++ lib/ui/pages/auth/register/register_page_screen.dart
@@ -1,4 +1,10 @@
 import 'package:arxivinder/blocs/bottom_nav_bar_bloc.dart';
+import 'package:arxivinder/blocs/register/register_auth_bloc.dart';
+import 'package:arxivinder/blocs/register/register_auth_event.dart';
+import 'package:arxivinder/blocs/register/state_register_auth.dart';
+import 'package:arxivinder/data/model/auth_request.dart';
+import 'package:arxivinder/data/services/auth_api.dart';
+import 'package:arxivinder/ui/pages/auth/login/login_page_screen.dart';
 import 'package:arxivinder/ui/pages/root/root_navigation_screen.dart';
 import 'package:arxivinder/ui/utils/auth_button.dart';
 import 'package:arxivinder/ui/utils/custom_textfield.dart';
@@ -15,138 +21,215 @@ class RegisterPageScreen extends StatefulWidget {
 }
 
 class RegisterPageState extends State<RegisterPageScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              clipBehavior: Clip.none,
-              alignment: Alignment.topCenter,
-              children: [
-                Container(
-                  width: 439,
-                  height: 567,
-                  decoration: const ShapeDecoration(
-                    color: Color.fromRGBO(54, 116, 181, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(32),
-                        bottomRight: Radius.circular(32),
+    return BlocProvider(
+      create: (context) => RegisterAuthBloc(authApi: AuthApi()),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.topCenter,
+                children: [
+                  Container(
+                    width: 439,
+                    height: 567,
+                    decoration: const ShapeDecoration(
+                      color: Color.fromRGBO(54, 116, 181, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(32),
+                          bottomRight: Radius.circular(32),
+                        ),
                       ),
                     ),
                   ),
-                ),
 
-                Column(
-                  children: [
-                    const SizedBox(height: 60),
-                    Image.asset(
-                      'assets/images/arxiv_register_logo.png',
-                      width: 124,
-                      height: 124,
-                      fit: BoxFit.contain,
-                    ),
-                    const SizedBox(height: 20),
-
-                    const Text(
-                      'Registrasi ke\nakun Anda',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 32.0,
-                        fontWeight: FontWeight.w700,
-                        height: 1.1,
-                        letterSpacing: -0.5,
-                        fontFamily: 'Roboto',
+                  Column(
+                    children: [
+                      const SizedBox(height: 60),
+                      Image.asset(
+                        'assets/images/arxiv_register_logo.png',
+                        width: 124,
+                        height: 124,
+                        fit: BoxFit.contain,
                       ),
-                    ),
-                    const SizedBox(height: 30),
+                      const SizedBox(height: 20),
 
-                    Container(
-                      width: 384,
-                      height: 478,
-                      padding: const EdgeInsets.all(32.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(32),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color(0x19000000),
-                            blurRadius: 10,
-                            offset: const Offset(0, 5),
-                          ),
-                        ],
+                      const Text(
+                        'Registrasi ke\nakun Anddasda',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32.0,
+                          fontWeight: FontWeight.w700,
+                          height: 1.1,
+                          letterSpacing: -0.5,
+                          fontFamily: 'Roboto',
+                        ),
                       ),
-                      child: Column(
-                        children: [
-                          Text("username"),
-                          Positioned(child: AuthTextField(hintText: "Halo")),
+                      const SizedBox(height: 30),
 
-                          SizedBox(height: 20),
-                          const Text(
-                            "Kata Sandi",
-                            style: TextStyle(color: Colors.black),
-                          ),
+                      Container(
+                        width: 384,
+                        height: 478,
+                        padding: const EdgeInsets.all(32.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(32),
+                          boxShadow: [
+                            BoxShadow(
+                              color: const Color(0x19000000),
+                              blurRadius: 10,
+                              offset: const Offset(0, 5),
+                            ),
+                          ],
+                        ),
+                        child: BlocConsumer<
+                          RegisterAuthBloc,
+                          StateRegisterAuth
+                        >(
+                          listener: (context, state) {
+                            if (state is RegisterSuccess) {
+                              _usernameController.clear();
+                              _passwordController.clear();
+                              Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  builder:
+                                      (context) => BlocProvider(
+                                        create: (_) => BottomNavBarBloc(),
+                                        child: const RootNavigationScreen(),
+                                      ),
+                                ),
+                                (route) => false,
+                              );
 
-                          SizedBox(height: 20),
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text(state.msg)),
+                              );
+                            } else if (state is RegisterFailure) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text(state.error)),
+                              );
 
-                          AuthTextField(hintText: "Hali", isPassword: true),
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (context) => const LoginPageScreen(),
+                                ),
+                              );
+                            }
+                          },
+                          builder: (context, state) {
+                            return Column(
+                              children: [
+                                const Text("Username"),
+                                AuthTextField(
+                                  controller: _usernameController,
+                                  hintText: "Masukkan username",
+                                ),
 
-                          SizedBox(height: 30),
-                          AuthButton(
-                            caption: "Registrasi",
-                            action:
-                                () => {
-                                  Navigator.pushAndRemoveUntil(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder:
-                                          (context) => BlocProvider(
-                                            create:
-                                                (context) => BottomNavBarBloc(),
-                                            child: const RootNavigationScreen(),
+                                const SizedBox(height: 20),
+                                const Text(
+                                  "Kata Sandi",
+                                  style: TextStyle(color: Colors.black),
+                                ),
+
+                                const SizedBox(height: 20),
+
+                                AuthTextField(
+                                  controller: _passwordController,
+                                  hintText: "Masukkan password",
+                                  isPassword: true,
+                                ),
+
+                                const SizedBox(height: 30),
+                                if (state is RegisterLoading)
+                                  const CircularProgressIndicator()
+                                else
+                                  AuthButton(
+                                    caption: "Registrasi",
+                                    action: () {
+                                      final username =
+                                          _usernameController.text.trim();
+                                      final password =
+                                          _passwordController.text.trim();
+
+                                      if (username.isEmpty ||
+                                          password.isEmpty) {
+                                        ScaffoldMessenger.of(
+                                          context,
+                                        ).showSnackBar(
+                                          const SnackBar(
+                                            content: Text(
+                                              'Username dan password harus diisi',
+                                            ),
                                           ),
-                                    ),
-                                    (route) => false,
-                                  ),
-                                },
-                          ),
+                                        );
+                                        return;
+                                      }
 
-                          SizedBox(height: 30),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Sudah Punya akun ?",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w500,
+                                      final authRequest = AuthRequest(
+                                        username: username,
+                                        password: password,
+                                      );
+
+                                      context.read<RegisterAuthBloc>().add(
+                                        RegisterSubmitted(
+                                          authRequest: authRequest,
+                                        ),
+                                      );
+                                    },
+                                  ),
+
+                                const SizedBox(height: 30),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      "Sudah Punya akun ?",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 4),
+                                    const Text(
+                                      "Login",
+                                      style: TextStyle(
+                                        fontFamily: "Roboto",
+                                        fontSize: 12,
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                              const SizedBox(width: 4),
-                              Text(
-                                "Registrasi",
-                                style: TextStyle(
-                                  fontFamily: "Roboto",
-                                  fontSize: 12,
-                                  color: Colors.blue,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
+                              ],
+                            );
+                          },
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 50),
-          ],
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
         ),
       ),
     );
