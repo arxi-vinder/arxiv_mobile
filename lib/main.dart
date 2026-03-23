766c5d131a17abd72d67756e3fc746d6064225a2
KrishnanditoLksn <krislaksana123@gmail.com>
2026-03-23 19:40:49 +0700

    feat:Registered recommender bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:arxivinder/blocs/bottom_nav_bar_bloc.dart';
 import 'package:arxivinder/blocs/papers/detail/detail_paper_bloc.dart';
 import 'package:arxivinder/blocs/papers/paper_bloc.dart';
+import 'package:arxivinder/blocs/papers/recommendation/recommender_bloc.dart';
 import 'package:arxivinder/data/services/detail_paper_api.dart';
 import 'package:arxivinder/data/services/papers_api.dart';
+import 'package:arxivinder/data/services/recomm_api.dart';
 import 'package:arxivinder/ui/pages/root/root_navigation_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -21,6 +23,7 @@ class MainApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (_) => BottomNavBarBloc()),
+        BlocProvider(create: (_)=> RecommenderBloc(api:RecommApi())),
         BlocProvider(create: (_) => PaperBloc(api: PapersApi())),
         BlocProvider(create: (_) => DetailPaperBloc(api: DetailPaperApi())),
       ],
