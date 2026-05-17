29a34cfb4f07a4ad50faf1e643354911d4570d0f
KrishnanditoLksn <krislaksana123@gmail.com>
2026-05-17 22:25:13 +0700

    feat:add unauthorized exception

--- lib/data/services/recomm_api.dart
+++ lib/data/services/recomm_api.dart
@@ -6,6 +6,14 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
+class UnauthorizedException implements Exception {
+  final String message;
+  UnauthorizedException([this.message = 'Unauthorized']);
+
+  @override
+  String toString() => message;
+}
+
 class RecommApi {
   final String? baseurl = dotenv.env['BASEURL'];
 
@@ -45,12 +53,17 @@ class RecommApi {
           .map((item) => PaperRecommendationResponse.fromJson(item))
           .toList()
         ..sort((a, b) => b.ucbScore.compareTo(a.ucbScore));
+    } else if (response.statusCode == 401) {
+      debugPrint('ERROR: Unauthorized (401)');
+      throw UnauthorizedException('Sesi Anda telah habis');
     } else {
       debugPrint('ERROR: Status code ${response.statusCode}');
       throw Exception(
         'Gagal mengambil rekomendasi paper: ${response.statusCode}',
       );
     }
+  } on UnauthorizedException {
+    rethrow;
   } catch (e) {
     debugPrint('Error: $e');
     throw Exception('Terjadi kesalahan: $e');
