9821466366dafb5ad4914cd5225ce659352e0e75
KrishnanditoLksn <krislaksana123@gmail.com>
2026-05-23 23:46:43 +0700

    refactor : change error message to english

--- lib/data/services/category_api.dart
+++ lib/data/services/category_api.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:arxivinder/data/model/category_response.dart';
 import 'package:arxivinder/data/model/paper_response.dart';
+import 'package:arxivinder/data/services/secure_storage_service.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
@@ -13,19 +14,21 @@ class CategoryApi {
     final url = Uri.parse('$baseurl/api/v1/categories');
 
     try {
-      final response = await http.get(
-        url,
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        },
-      ).timeout(
-        const Duration(seconds: 10),
-        onTimeout: () {
-          debugPrint('ERROR: Request timeout');
-          throw Exception('Request timeout');
-        },
-      );
+      final response = await http
+          .get(
+            url,
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+            },
+          )
+          .timeout(
+            const Duration(seconds: 10),
+            onTimeout: () {
+              debugPrint('ERROR: Request timeout');
+              throw Exception('Request timeout');
+            },
+          );
 
       if (response.statusCode >= 200 && response.statusCode < 300) {
         final Map<String, dynamic> datas = jsonDecode(response.body);
@@ -33,50 +36,54 @@ class CategoryApi {
         return dataList.map((item) => CategoryResponse.fromJson(item)).toList();
       } else {
         final Map<String, dynamic> errorData = jsonDecode(response.body);
-        throw Exception(
-          errorData['message'] ?? 'Gagal mengambil kategori',
-        );
+        throw Exception(errorData['message'] ?? 'Failed to fetch categories');
       }
     } catch (e) {
       debugPrint('Error: $e');
-      throw Exception('Terjadi kesalahan: $e');
+      throw Exception('An error occurred: $e');
     }
   }
 
   Future<List<PaperResponse>> getPapersByCategory(String categoryName) async {
     final encodedCategory = Uri.encodeComponent(categoryName);
     final url = Uri.parse('$baseurl/api/v1/papers/category/$encodedCategory');
-
+    final token = await SecureStorageService.getAccessToken();
     try {
-      final response = await http.get(
-        url,
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        },
-      ).timeout(
-        const Duration(seconds: 10),
-        onTimeout: () {
-          debugPrint('ERROR: Request timeout');
-          throw Exception('Request timeout');
-        },
-      );
+      final response = await http
+          .get(
+            url,
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+              'Authorization': 'Bearer $token',
+            },
+          )
+          .timeout(
+            const Duration(seconds: 10),
+            onTimeout: () {
+              debugPrint('ERROR: Request timeout');
+              throw Exception('Request timeout');
+            },
+          );
 
       if (response.statusCode >= 200 && response.statusCode < 300) {
         final Map<String, dynamic> datas = jsonDecode(response.body);
         final List<dynamic> dataList = datas['data'] ?? [];
         return dataList
-            .map((item) => PaperResponse.fromJson({'data': item, 'status': 'success'}))
+            .map(
+              (item) =>
+                  PaperResponse.fromJson({'data': item, 'status': 'success'}),
+            )
             .toList();
       } else {
         final Map<String, dynamic> errorData = jsonDecode(response.body);
         throw Exception(
-          errorData['message'] ?? 'Gagal mengambil paper kategori',
+          errorData['message'] ?? 'Failed to fetch category papers',
         );
       }
     } catch (e) {
       debugPrint('Error: $e');
-      throw Exception('Terjadi kesalahan: $e');
+      throw Exception('An error occurred: $e');
     }
   }
 }
